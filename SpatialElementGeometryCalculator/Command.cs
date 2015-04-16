295cbe084a7c69f03d0745cb9176436da70c8a9e
Håkon Clausen <hakonhc@hclausen.net>
2015-04-16 19:44:27 +0200

    Rework to fix two problems: 1) Do not remove inserts for other room 2) Only withdraw inserts on a wall one time

--- SpatialElementGeometryCalculator/Command.cs
+++ SpatialElementGeometryCalculator/Command.cs
@@ -1,234 +1,127 @@
-#region Namespaces
+#region
+
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
+
 #endregion
 
 namespace SpatialElementGeometryCalculator
 {
-  [Transaction( TransactionMode.Manual )]
-  public class Command : IExternalCommand
-  {
-    /// <summary>
-    /// Convert square feet to square meters 
-    /// with two decimal places precision.
-    /// </summary>
-    double sqFootToSquareM( double sqFoot )
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
     {
-      return Math.Round( sqFoot * 0.092903, 2 );
-    }
-
-    /// <summary>
-    /// Calculate wall area minus openings. Temporarily
-    /// delete all openings in a transaction that is
-    /// rolled back.
-    /// </summary>
-    /// <param name="subfaceArea">Initial gross subface area</param>
-    /// <param name="wall"></param>
-    /// <param name="doc"></param>
-    /// <param name="room"></param>
-    /// <returns></returns>
-    double calwallAreaMinusOpenings(
-      double subfaceArea,
-      Element wall,
-      Room room )
-    {
-      Document doc = wall.Document;
-
-      // Is this a reliable way to compare documents?
-
-      Debug.Assert(
-        room.Document.ProjectInformation.UniqueId.Equals(
-          doc.ProjectInformation.UniqueId ),
-        "expected wall and room from same document" );
-
-      // Determine all openings in the given wall.
-
-#if DEBUG
-      FilteredElementCollector fiCol
-        = new FilteredElementCollector( doc )
-          .OfClass( typeof( FamilyInstance ) );
-
-      List<ElementId> lstTotempDel
-        = new List<ElementId>();
-
-      foreach( FamilyInstance fi in fiCol )
-      {
-        // The family instances hosted by this wall
-        // could be filtered out more efficiently using 
-        // a filtered element collector parameter filter.
-        // This would be important in a large model.
-
-        if( fi.get_Parameter(
-          BuiltInParameter.HOST_ID_PARAM )
-            .AsElementId().IntegerValue.Equals(
-              wall.Id.IntegerValue ) )
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-          if( ( fi.Room != null )
-            && ( fi.Room.Id == room.Id ) )
-          {
-            lstTotempDel.Add( fi.Id );
-          }
-          else if( ( fi.FromRoom != null )
-            && ( fi.FromRoom.Id == room.Id ) )
-          {
-            lstTotempDel.Add( fi.Id );
-          }
-          else if( ( fi.ToRoom != null )
-            && ( fi.ToRoom.Id == room.Id ) )
-          {
-            lstTotempDel.Add( fi.Id );
-          }
-        }
-      }
-#endif // DEBUG
-
-      // FindInserts is much more efficient and 
-      // entirely avoids the use of all filtered 
-      // element collectors.
-
-      IList<ElementId> inserts = ( wall as HostObject )
-        .FindInserts( true, true, true, true );
-
-      // In the case of a compound wall, FindInserts
-      // will return the openings, whereas the filtered
-      // element collector will not.
-
-      Debug.Assert(
-        lstTotempDel.Count <= inserts.Count,
-        "expected FindInserts to return all openings" );
-
-      // Determine total area of all openings.
-
-      double openingArea = 0;
-
-      if( 0 < inserts.Count )
-      {
-        Transaction t = new Transaction( doc );
-
-        double wallAreaNet = wall.get_Parameter(
-          BuiltInParameter.HOST_AREA_COMPUTED )
-            .AsDouble();
-
-        t.Start( "tmp Delete" );
-        doc.Delete( inserts );
-        doc.Regenerate();
-        double wallAreaGross = wall.get_Parameter(
-          BuiltInParameter.HOST_AREA_COMPUTED )
-            .AsDouble();
-        t.RollBack();
-
-        openingArea = wallAreaGross - wallAreaNet;
-      }
-
-      return subfaceArea - openingArea;
-    }
-
-    public Result Execute(
-      ExternalCommandData commandData,
-      ref string message,
-      ElementSet elements )
-    {
-      UIApplication app = commandData.Application;
-      Document doc = app.ActiveUIDocument.Document;
-
-      SpatialElementBoundaryOptions sebOptions
-        = new SpatialElementBoundaryOptions();
-
-      sebOptions.SpatialElementBoundaryLocation
-        = SpatialElementBoundaryLocation.Finish;
-
-      Result rc;
-
-      try
-      {
-        FilteredElementCollector roomCol
-          = new FilteredElementCollector( doc )
-            .OfClass( typeof( SpatialElement ) );
-
-        string s = "Finished populating Rooms with "
-          + "Boundary Data\r\n\r\n";
-
-        foreach( SpatialElement e in roomCol )
-        {
-          Room room = e as Room;
-
-          if( room != null )
-          {
+            var app = commandData.Application;
+            var doc = app.ActiveUIDocument.Document;
+            Result rc;
+            string s = string.Empty;
             try
             {
-              Autodesk.Revit.DB
-                .SpatialElementGeometryCalculator
-                  calc = new Autodesk.Revit.DB
-                    .SpatialElementGeometryCalculator(
-                      doc, sebOptions );
-
-              SpatialElementGeometryResults results
-                = calc.CalculateSpatialElementGeometry(
-                  room );
-
-              Solid roomSolid = results.GetGeometry();
-
-              foreach( Face face in roomSolid.Faces )
-              {
-                IList<SpatialElementBoundarySubface>
-                  subfaceList = results.GetBoundaryFaceInfo(
-                    face );
-
-                foreach( SpatialElementBoundarySubface
-                  subface in subfaceList )
+                var roomCol = new FilteredElementCollector(doc).OfClass(typeof(SpatialElement));
+ 
+                foreach (var e in roomCol)
                 {
-                  if( subface.SubfaceType
-                    == SubfaceType.Side )
-                  {
-                    Element wall = doc.GetElement(
-                      subface.SpatialBoundaryElement
-                        .HostElementId );
+                    var room = e as Room;
+                    if (room == null) continue;
+                    if (room.Location == null) continue;
 
-                    double subfaceArea = subface
-                      .GetSubface().Area;
+                    var sebOptions = new SpatialElementBoundaryOptions { SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Finish };
+                    var calc = new Autodesk.Revit.DB.SpatialElementGeometryCalculator(doc, sebOptions);
+                    var results = calc.CalculateSpatialElementGeometry(room);
+                    
+                    // To keep track of each wall and its total area in the room
+                    var walls = new Dictionary<string, double>();
 
-                    double netArea = sqFootToSquareM(
-                      calwallAreaMinusOpenings(
-                        subfaceArea, wall, room ) );
+                    foreach (Face face in results.GetGeometry().Faces)
+                    {
+                        foreach (var subface in results.GetBoundaryFaceInfo(face))
+                        {
+                            if (subface.SubfaceType != SubfaceType.Side) { continue; }
+                            var wall = doc.GetElement(subface.SpatialBoundaryElement.HostElementId) as HostObject;
+                            if (wall == null) { continue; }
+                            var grossArea = subface.GetSubface().Area;
+                            if (!walls.ContainsKey(wall.UniqueId))
+                            {
+                                walls.Add(wall.UniqueId, grossArea);
+                            }
+                            else
+                            {
+                                walls[wall.UniqueId] += grossArea;
+                            }
+                        }
+                    }
 
-                    s = s + "Room "
-                      + room.get_Parameter(
-                        BuiltInParameter.ROOM_NUMBER )
-                          .AsString()
-                      + " : Wall " + wall.get_Parameter(
-                        BuiltInParameter.ALL_MODEL_MARK )
-                          .AsString()
-                      + " : Area " + netArea.ToString()
-                      + " m2\r\n";
-                  }
+                    foreach (var id in walls.Keys)
+                    {
+                        var wall = (HostObject)doc.GetElement(id);
+                        var openings = CalculateWallOpeningArea(wall, room);
+
+                        s += string.Format("Room: {2} Wall: {0} Area: {1} m2\r\n", 
+                            wall.get_Parameter(BuiltInParameter.ALL_MODEL_MARK).AsString(), 
+                            SqFootToSquareM(walls[id] - openings), 
+                            room.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString());
+                    }
+                 
                 }
-              }
-              s = s + "\r\n";
+
+                TaskDialog.Show("Room Boundaries", s);
+                rc = Result.Succeeded;
             }
-            catch( Exception )
+            catch (Exception ex)
             {
+                TaskDialog.Show("Room Boundaries",ex.Message + "\r\n"+ ex.StackTrace);
+                rc = Result.Failed;
             }
-          }
+
+            return rc;
         }
-        TaskDialog.Show( "Room Boundaries", s );
 
-        rc = Result.Succeeded;
-      }
-      catch( Exception ex )
-      {
-        TaskDialog.Show( "Room Boundaries",
-          ex.Message.ToString() + "\r\n"
-          + ex.StackTrace.ToString() );
+        /// <summary>
+        ///     Convert square feet to square meters
+        ///     with two decimal places precision.
+        /// </summary>
+        private static double SqFootToSquareM(double sqFoot)
+        {
+            return Math.Round(sqFoot*0.092903, 2);
+        }
 
-        rc = Result.Failed;
-      }
-      return rc;
+        /// <summary>
+        ///     Calculate wall area minus openings. Temporarily
+        ///     delete all openings in a transaction that is
+        ///     rolled back.
+        /// </summary>
+        /// <param name="wall"></param>
+        /// <param name="room"></param>
+        private static double CalculateWallOpeningArea(HostObject wall, Room room)
+        {
+            var doc = wall.Document;
+            var wallAreaNet = wall.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED).AsDouble();
+            var t = new Transaction(doc);
+            t.Start("Temp");
+            foreach (var id in wall.FindInserts(true, true, true, true))
+            {
+                var insert = doc.GetElement(id);
+                if (insert is FamilyInstance && IsInRoom(room, (FamilyInstance)insert))
+                {
+                    doc.Delete(id);
+                }
+            }
+
+            doc.Regenerate();
+            var wallAreaGross = wall.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED).AsDouble();
+            t.RollBack();
+
+            return wallAreaGross - wallAreaNet;
+        }
+
+        private static bool IsInRoom(Room room, FamilyInstance f)
+        {
+            return ((f.Room != null && f.Room.Id == room.Id) || (f.ToRoom != null && f.ToRoom.Id == room.Id) || (f.FromRoom != null && f.FromRoom.Id == room.Id));
+        }
     }
-  }
 }
